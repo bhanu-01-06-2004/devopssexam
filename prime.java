8aa19cb4ae99a290e54e63fa8837412f0f3246fc
bhanu <22h51a05k4@cmrcet.ac.in>
2024-11-19 18:41:46 +0530

    changes

--- prime.java
+++ prime.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class Prime {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int num = scanner.nextInt();
+        int num = 5;
 
         // Handle numbers less than or equal to 1
         if (num <= 1) {
