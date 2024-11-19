f4148a52eba159991aada7eb9414141f310aae95
bhanu <22h51a05k4@cmrcet.ac.in>
2024-11-19 17:55:16 +0530

    Added user input for prime number checking

--- prime.java
+++ prime.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Prime {
     public static void main(String[] args) {
-        int num = 29;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
         boolean isPrime = true;
 
         for (int i = 2; i <= num / 2; i++) {
