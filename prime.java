e19a0fa0132ead78747fc174bb1a276ff4271808
bhanu <22h51a05k4@cmrcet.ac.in>
2024-11-19 18:19:45 +0530

    Added

--- prime.java
+++ prime.java
@@ -5,22 +5,28 @@ public class Prime {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int num = scanner.nextInt();
+
+        // Handle numbers less than or equal to 1
+        if (num <= 1) {
+            System.out.println(num + " is not a prime number.");
+            return;
+        }
+
         boolean isPrime = true;
 
-       for (int i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i == 0) {
-        isPrime = false;
-        break;
-    }
-}
+        // Check for prime using optimized loop
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
 
-// This program checks if a number is prime or not
+        // Output the result
         if (isPrime) {
             System.out.println(num + " is a prime number.");
-        }if (num <= 1) {
-    System.out.println(num + " is not a prime number.");
-    return;
-}
-
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
     }
 }
